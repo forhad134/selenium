8592572f62bd119ce7ae5cb05ba1f07f1ab23893
ahmedforhad <aq182974@gmail.com>
2023-10-20 00:46:41 -0400

    loop

---
+++ project.selenium/src/main/java/practice/Loop.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Loop {
+
+		public static void main(String[] args) {
+			String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+			for (String i : cars) {
+				System.out.println(i);
+			}
+	}
+
+}
